4d81fb2feeeb4d4a834268824e17445715c2e696
Giovani Zaghi <giovanizaghinogueira@outlook.com>
2020-09-27 19:51:06 -0300

    Implementação inicial do GridView

--- lib/views/productsOverviewScreen.dart
+++ lib/views/productsOverviewScreen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/data/dummyData.dart';
+import 'package:loja_virtual_flutter/models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadesProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Minha Loja"),
+        ),
+        body: GridView.builder(
+          padding: const EdgeInsets.all(10),
+          itemCount: loadesProducts.length,
+          itemBuilder: (ctx, i) => Text(loadesProducts[i].title),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ));
   }
 }

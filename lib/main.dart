1bd17e08639c3e60effe14d7259b1c7d1cfb1f74
Giovani Zaghi <giovanizaghinogueira@outlook.com>
2020-10-04 12:00:21 -0300

    Provider de produtos com ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/utils/appRoutes.dart';
 import 'package:shop/views/productDetailScreen.dart';
 import 'package:shop/views/productsOverviewScreen.dart';
+
+import 'providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: "Lato",
+    return ChangeNotifierProvider(
+      create: (_) => new Products(),
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/views/productsOverviewScreen.dart
+++ lib/views/productsOverviewScreen.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummyData.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/productItem.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
-
   @override
   Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Minha Loja"),
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ProductItem(products[i]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,

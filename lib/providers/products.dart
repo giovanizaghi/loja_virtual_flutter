d717a2aac57e7668abed56f90db337389ad472ef
Giovani Zaghi <giovanizaghinogueira@outlook.com>
2020-10-04 13:20:36 -0300

    Filtragem de favoritos

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -7,8 +7,33 @@ class Products with ChangeNotifier {
 
   List<Product> get items => [..._items];
 
+  List<Product> get favoriteItems {
+    return _items.where((element) => element.isFavorite).toList();
+  }
+
   void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
   }
 }
+
+//utilizar código abaixo caso queira configurar o filtro de forma global
+
+// bool _showFavoriteOnly = false;
+
+// List<Product> get items {
+//   if (_showFavoriteOnly) {
+//     return _items.where((prod) => prod.isFavorite).toList();
+//   }
+//   return [..._items];
+// }
+
+// void showFavoriteOnly() {
+//   _showFavoriteOnly = true;
+//   notifyListeners();
+// }
+
+// void showAll() {
+//   _showFavoriteOnly = false;
+//   notifyListeners();
+// }
--- lib/views/productGrid.dart
+++ lib/views/productGrid.dart
@@ -4,10 +4,16 @@ import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/productItem.dart';
 
 class ProductGrid extends StatelessWidget {
+  final bool showFavoriteOnly;
+
+  ProductGrid(this.showFavoriteOnly);
+
   @override
   Widget build(BuildContext context) {
     final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
+    final products = showFavoriteOnly
+        ? productsProvider.favoriteItems
+        : productsProvider.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10),
